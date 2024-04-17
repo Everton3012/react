f64265e1c5dd399c31cb8d73aef2705e94b8bbaa
Everton Brandão de Moura <evertonbrandao137@hotmail.com>
2024-04-16 21:09:39 -0300

    useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
+import { useState } from "react";
 import { Nome } from "./components/Nome";
 
 const App =() => {
+  const [aluno, setAluno] = useState("Ronald");
+
+  function handleChangeName(){
+    setAluno("Kaue");
+  }
+
   return (
     <>
       <h1>Bem vindo ao meu projeto</h1>
+      <br />
+      <h2>Olá: {aluno}</h2>
+      <br />
+      <button onClick={handleChangeName}>
+        Mudar nome
+      </button>
+      <br />
       <Nome aluno="Everton"/>
       <br />
       <Nome aluno="David"/>
