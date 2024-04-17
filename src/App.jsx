b3fc7f35b8ef66c5d4831489c595cf364920b84f
Everton Brandão de Moura <evertonbrandao137@hotmail.com>
2024-04-16 21:04:12 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Nome } from "./components/Nome";
+
+const App =() => {
+  return (
+    <>
+      <h1>Bem vindo ao meu projeto</h1>
+      <Nome aluno="Everton"/>
+      <br />
+      <Nome aluno="David"/>
+    </>
+  )
+}
+
+export default App;
---
+++ src/components/Nome.jsx
@@ -0,0 +1,6 @@
+export const Nome = ({aluno}) => {
+    return(
+      <span>Bem Vindo: {aluno}</span>
+    )
+  }
+  
